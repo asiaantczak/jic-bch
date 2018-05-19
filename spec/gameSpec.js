7c013c3a03eb7cd54694a2e801cfc3719edfcdff
Asia Antczak <aasiaantczak@gmail.com>
2018-05-19 16:39:35 +0100

    removed some of the tests - simplify the functions

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,29 +1,17 @@
 describe("Game", function() {
+  debugger;
   var game;
   const max_pins = 10;
 
 
   beforeEach(function() {
     game = new Game();
-
   });
 
   describe("Roll", function(){
     it("adds knocked pins to frame", function() {
       game.roll(6);
       expect(game.frame).toEqual([6]);
-    });
-
-    it("can knock max of 10 pins in one roll", function() {
-      expect(function() { game.roll(11) }).toThrow("There is max of 10 pins to knock");
-    });
-
-    it("cannot roll more than 2 times", function() {
-      game.roll(5);
-      game.roll(3);
-      game.roll(1);
-      expect(game.frame.length).toEqual(2);
-      expect(game.frame).toEqual[5,3];
     });
   });
 
@@ -33,12 +21,6 @@ describe("Game", function() {
       game.roll(4);
       game.addAFrame([2, 4]);
       expect(game.frames).toEqual([[2, 4]]);
-    });
-
-    it("cannot add more than 10 frames", function() {
-      game.frames.length = 10
-      game.addAFrame([1, 3]);
-      expect(game.frames.length).toEqual(10);
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -6,20 +6,13 @@ function Game() {
 }
 
 Game.prototype.roll = function(pins) {
-  if (pins > 10) {
-    throw "There is max of 10 pins to knock";
-  }
-  if (this.frame.length < 2) {
     this.frame.push(pins);
-  }
 };
 
 
 Game.prototype.addAFrame = function(frame) {
-  if(this.frames.length < 10) {
     this.frames.push(frame);
-    this.resetFrame(frame);
-  }
+    this.resetFrame();
 };
 
 Game.prototype.resetFrame = function(frame) {
